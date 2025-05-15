e7dca64568d42b0de1467b2b5a7fe95f92b20821
BaldMan4 <LaanTristan1@hotmail.com>
2025-05-15 14:50:10 +0200

    added creative mod tab

--- src/main/java/net/baldman/everythingmod/EverythingMod.java
+++ src/main/java/net/baldman/everythingmod/EverythingMod.java
@@ -1,5 +1,6 @@
 package net.baldman.everythingmod;
 
+import net.baldman.everythingmod.item.ModCreativeModeTabs;
 import net.baldman.everythingmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -35,6 +36,8 @@ public class EverythingMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
 
